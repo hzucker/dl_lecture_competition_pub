7d427e0b2942dc8c9ed1e433b5981d1f3ff28794
hzucker4gci <hzucker4gci@gmail.com>
2024-07-13 23:37:37 +0900

    model = VQAModelを前にもってくる

--- main.py
+++ main.py
@@ -467,6 +467,9 @@ def main():
         transforms.ToTensor()
 #        GCN
     ])
+
+    model = VQAModel(vocab_size=len(train_dataset.question2idx)+1, n_answer=len(train_dataset.answer2idx)).to(device)
+
     train_dataset = VQADataset(df_path="./data/train.json", image_dir="./data/train", transform=transform)
     test_dataset = VQADataset(df_path="./data/valid.json", image_dir="./data/valid", transform=transform, answer=False)
     test_dataset.update_dict(train_dataset)
@@ -474,7 +477,6 @@ def main():
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
 
-    model = VQAModel(vocab_size=len(train_dataset.question2idx)+1, n_answer=len(train_dataset.answer2idx)).to(device)
 
     # optimizer / criterion
     num_epoch = 1
