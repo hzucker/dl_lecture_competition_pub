7dfc3e7d2a0c8dff5a6e551b948437143e4bd56b
hzucker4gci <hzucker4gci@gmail.com>
2024-07-15 13:54:15 +0900

    zero->zeros

--- main.py
+++ main.py
@@ -217,7 +217,7 @@ class VQADataset(torch.utils.data.Dataset):
                 question_vector.append(np.zeros(self.w2v_model.vector_size))
 
         while len(question_vector) < self.max_qlen:
-            question_vector.append(np.zero(100))
+            question_vector.append(np.zeros(100))
 
 
 
