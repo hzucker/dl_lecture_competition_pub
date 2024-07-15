b721c540d14a65964283e18f7ae13ea955d80f96
hzucker4gci <hzucker4gci@gmail.com>
2024-07-15 20:41:24 +0900

    計算時間短縮

--- main.py
+++ main.py
@@ -284,18 +284,25 @@ def VQA_criterion(batch_pred: torch.Tensor, batch_answers: torch.Tensor):
     total_acc = 0.
 
     for pred, answers in zip(batch_pred, batch_answers):
-        acc = 0.
-        for i in range(len(answers)):
-            num_match = 0
-            for j in range(len(answers)):
-                if i == j:
-                    continue
-                if pred == answers[j]:
-                    num_match += 1
-            acc += min(num_match / 3, 1)
-        total_acc += acc / 10
+        # 各回答ごとの一致数を計算
+        matches = (pred.unsqueeze(0) == answers).sum(dim=1)
+        # 各回答ごとの正答率を計算
+        acc = (min(m / 3, 1) for m in matches)
+        total_acc += sum(acc) / 10
 
     return total_acc / len(batch_pred)
+#        acc = 0.
+#        for i in range(len(answers)):
+#            num_match = 0
+#            for j in range(len(answers)):
+#                if i == j:
+#                    continue
+#                if pred == answers[j]:
+#                    num_match += 1
+#            acc += min(num_match / 3, 1)
+#        total_acc += acc / 10
+
+#    return total_acc / len(batch_pred)
 
 
 # 3. モデルのの実装
